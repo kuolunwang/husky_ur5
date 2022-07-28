0a437e0a5ffe37f4d01fbf5d853e283af2bf8c39
kuolunwang <kuolunwang.ee08@nycu.edu.tw>
2022-07-28 11:46:18 +0800

    add contact sensor and update husky_ur5 control code

--- husky_ur5_bringup/src/husky_ur5_control.py
+++ husky_ur5_bringup/src/husky_ur5_control.py
@@ -12,13 +12,27 @@ class husky_ur5:
 
         self.set_init_pose_srv = rospy.ServiceProxy("/gazebo/set_model_state", SetModelState)
         self.arm_home_srv = rospy.ServiceProxy("/robot/ur5/go_home", Trigger)
-        rospy.Service("/husky_ur5/random", Trigger, self.setting)
-        rospy.Service("/husky_ur5/pull_random", Trigger, self.setting_pull)
-        rospy.Service("/husky_ur5/init", Trigger, self.go_init)
-        rospy.Service("/husky/init", Trigger, self.go_init_husky)
+
+        # For husky ur5 ex2
+        rospy.Service("/husky_ur5/random", Trigger, self.go_husky_ur5_init)
+
+        # For ex3 box
+        rospy.Service("/husky_ur5/pull_box", Trigger, self.pull_box_init)
+
+        # For ex3 cardboard
+        rospy.Service("/husky_ur5/pull_cardboard", Trigger, self.pull_cardboard_init)
+
+        # For husky vx300s ex2
+        rospy.Service("/husky_vx300s/init", Trigger, self.go_husky_vx300s_init)
+
+        # For husky ur5 ex1 
+        rospy.Service("/husky_ur5/init", Trigger, self.go_ex1_init)
+
+        # For pokingbot ex1
+        rospy.Service("/husky/init", Trigger, self.go_pokinbbot_init)
         self.set_door_srv = rospy.ServiceProxy("/gazebo/set_model_configuration", SetModelConfiguration)
 
-    def go_init_husky(self, req):
+    def go_pokinbbot_init(self, req):
 
         res = TriggerResponse()
 
@@ -42,7 +56,35 @@ class husky_ur5:
 
         return res
 
-    def go_init(self, req):
+    def go_husky_vx300s_init(self, req):
+
+        res = TriggerResponse()
+
+        req = ModelState()
+        req.model_name = 'robot'
+        req.pose.position.x = 9.0 + random.uniform(-0.1, 0.1)
+        req.pose.position.y = 13.3 + random.uniform(-0.1, 0.1)
+        req.pose.position.z = 0.1323
+
+        angle = random.uniform(-95, -85)
+        r = R.from_euler("z", angle, degrees=True)
+        quat = r.as_quat()
+        req.pose.orientation.x = quat[0]
+        req.pose.orientation.y = quat[1]
+        req.pose.orientation.z = quat[2]
+        req.pose.orientation.w = quat[3]
+
+        # set robot
+        self.set_init_pose_srv(req)
+
+        # set door 
+        self.set_door_srv("hinge_door_0", "", ["hinge"], [0])
+
+        res.success = True
+
+        return res
+
+    def go_ex1_init(self, req):
 
         res = TriggerResponse()
 
@@ -61,7 +103,7 @@ class husky_ur5:
 
         # set door 
         self.set_door_srv("hinge_door_0", "", ["hinge"], [0])
-        
+
         # arm go home
         self.arm_home_srv()
 
@@ -69,7 +111,7 @@ class husky_ur5:
 
         return res
         
-    def setting(self, req):
+    def go_husky_ur5_init(self, req):
 
         res = TriggerResponse()
 
@@ -100,7 +142,7 @@ class husky_ur5:
 
         return res
 
-    def setting_pull(self, req):
+    def pull_box_init(self, req):
 
         res = TriggerResponse()
 
@@ -108,6 +150,29 @@ class husky_ur5:
         req.model_name = 'robot'
         req.pose.position.x = 9.0 + random.uniform(-0.1, 0.1)
         req.pose.position.y = 14.2 + random.uniform(-0.1, 0.1)
+        req.pose.position.z = 0.1323
+
+        angle = random.uniform(-95, -85)
+        r = R.from_euler("z", angle, degrees=True)
+        quat = r.as_quat()
+        req.pose.orientation.x = quat[0]
+        req.pose.orientation.y = quat[1]
+        req.pose.orientation.z = quat[2]
+        req.pose.orientation.w = quat[3]
+        self.set_init_pose_srv(req)
+
+        res.success = True
+
+        return res
+
+    def pull_cardboard_init(self, req):
+
+        res = TriggerResponse()
+
+        req = ModelState()
+        req.model_name = 'robot'
+        req.pose.position.x = 9.0 + random.uniform(-0.1, 0.1)
+        req.pose.position.y = 13.5 + random.uniform(-0.1, 0.1)
         req.pose.position.z = 0.1323
 
         angle = random.uniform(-95, -85)
